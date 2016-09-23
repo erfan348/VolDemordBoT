dd8e51185795f2237e85e72106f9c41e1a02f333
erfan348 <erfanmbaba@gmail.com>
2016-09-23 16:37:19 +0330

    Add files via upload

--- plugins/aparat.lua
+++ plugins/aparat.lua
@@ -1,21 +1,23 @@
-local function run(msg, matches)
- if matches[1]:lower() == 'aparat' then
-  local url = http.request('http://www.aparat.com/etc/api/videoBySearch/text/'..URL.escape(matches[2]))
-  local jdat = json:decode(url)
 
-  local items = jdat.videobysearch
-  text = 'نتیجه جستوجو در آپارات : \n'
-  for i = 1, #items do
-  text = text..'\n'..i..'- '..items[i].title..'  -  تعداد بازدید: '..items[i].visit_cnt..'\n    لینک: aparat.com/v/'..items[i].uid
-  end
-  text = text..'\n\n@EN_Security'
-  return text
- end
+
+local function run(msg, matches)
+	if matches[1]:lower() == 'aparat' then
+		local url = http.request('http://www.aparat.com/etc/api/videoBySearch/text/'..URL.escape(matches[2]))
+		local jdat = json:decode(url)
+
+		local items = jdat.videobysearch
+		text = 'نتیجه جستجو در آپارات: \n'
+		for i = 1, #items do
+		text = text..'\n\n'..i..'-'..items[i].title..'\nتعداد بازدید: '..items[i].visit_cnt..'\n   لینک: aparat.com/v/'..items[i].uid
+		end
+		text = text..'\n\n@cruel_channel'
+		return text
+	end
 end
 
 return {
    patterns = {
-"^[#/!](aparat) (.*)$",
+"^[/!](aparat) (.*)$",
    },
    run = run
 }
