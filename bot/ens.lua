0a7f8590e2a61351a8612518f4b28c98f84febc2
erfan348 <erfanmbaba@gmail.com>
2017-12-02 17:45:09 +0330

    Update ens.lua

--- bot/ens.lua
+++ bot/ens.lua
@@ -251,7 +251,7 @@ function create_config( )
     "sh",
     "serverinfo"
     },
-    sudo_users = {167626247},--Sudo users
+    sudo_users = {460596485},--Sudo users
     moderation = {data = 'data/moderation.json'},
     about_text = [[Electrovirus national security bot🌐
  E N S
